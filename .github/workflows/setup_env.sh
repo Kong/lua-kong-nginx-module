2a13edd59c938f96d6758507e3a90f474690bf82
chronolaw <chrono_cpp@me.com>
2022-02-18 13:11:06 +0800

    use two github env var

--- .github/workflows/setup_env.sh
+++ .github/workflows/setup_env.sh
@@ -39,7 +39,7 @@ kong-ngx-build \
     --work $DOWNLOAD_ROOT \
     --prefix $INSTALL_ROOT \
     --openresty $OPENRESTY \
-    --kong-nginx-module ${GITHUB_BASE_REF} \
+    --kong-nginx-module ${GITHUB_BASE_REF:-$GITHUB_REF_NAME} \
     --luarocks $LUAROCKS \
     --openssl $OPENSSL \
     --debug \
