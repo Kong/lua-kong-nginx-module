d2173a66d8a7364dc5aaa42fa3f557799359843b
chronolaw <chrono_cpp@me.com>
2022-02-18 17:32:08 +0800

    comments clean

--- .github/workflows/setup_env.sh
+++ .github/workflows/setup_env.sh
@@ -30,7 +30,6 @@ kong-ngx-build \
     --debug \
     -j $JOBS
 
-#export PATH=$LUAROCKS_INSTALL/bin:$PATH
 export PATH=$OPENSSL_INSTALL/bin:$OPENRESTY_INSTALL/nginx/sbin:$OPENRESTY_INSTALL/bin:$LUAROCKS_INSTALL/bin:$PATH
 
 eval `luarocks path`
