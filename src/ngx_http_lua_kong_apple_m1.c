b5b5d5a22a868004479b043cd80a0ce099a86312
chronolaw <chrono_cpp@me.com>
2022-03-21 18:09:31 +0800

    code clean for m1

--- src/ngx_http_lua_kong_apple_m1.c
+++ src/ngx_http_lua_kong_apple_m1.c
@@ -20,11 +20,6 @@
 
 // macOS with M1 fixes, see: https://github.com/LuaJIT/LuaJIT/issues/205
 
-int ngx_http_lua_ffi_shdict_get(ngx_shm_zone_t *zone, const unsigned char *key,
-    size_t key_len, int *value_type, unsigned char **str_value_buf,
-    size_t *str_value_len, double *num_value, int *user_flags,
-    int get_stale, int *is_stale, char **errmsg);
-
 typedef struct {
     ngx_shm_zone_t *zone;
     const unsigned char *key;
@@ -38,21 +33,6 @@ typedef struct {
     int *is_stale;
     char **errmsg;
 } ngx_shdict_get_t;
-
-int
-ngx_http_lua_ffi_shdict_get_m1(ngx_shdict_get_t *s)
-{
-    return ngx_http_lua_ffi_shdict_get(s->zone, s->key, s->key_len, s->value_type,
-        s->str_value_buf, s->str_value_len, s->num_value, s->user_flags, s->get_stale,
-        s->is_stale, s->errmsg);
-}
-
-
-int ngx_http_lua_ffi_shdict_store(ngx_shm_zone_t *zone, int op,
-    const unsigned char *key, size_t key_len, int value_type,
-    const unsigned char *str_value_buf, size_t str_value_len,
-    double num_value, long exptime, int user_flags, char **errmsg,
-    int *forcible);
 
 typedef struct {
     ngx_shm_zone_t *zone;
@@ -69,19 +49,6 @@ typedef struct {
     int *forcible;
 } ngx_shdict_store_t;
 
-int
-ngx_http_lua_ffi_shdict_store_m1(ngx_shdict_store_t *s)
-{
-    return ngx_http_lua_ffi_shdict_store(s->zone, s->op, s->key, s->key_len, s->value_type,
-        s->str_value_buf, s->str_value_len, s->num_value, s->exptime, s->user_flags, s->errmsg,
-        s->forcible);
-}
-
-
-int ngx_http_lua_ffi_shdict_incr(ngx_shm_zone_t *zone, const unsigned char *key,
-    size_t key_len, double *num_value, char **errmsg, int has_init,
-    double init, long init_ttl, int *forcible);
-
 typedef struct {
     ngx_shm_zone_t *zone;
     const unsigned char *key;
@@ -93,19 +60,6 @@ typedef struct {
     long init_ttl;
     int *forcible;
 } ngx_shdict_incr_t;
-
-int
-ngx_http_lua_ffi_shdict_incr_m1(ngx_shdict_incr_t *s)
-{
-    return ngx_http_lua_ffi_shdict_incr(s->zone, s->key, s->key_len, s->num_value,
-        s->errmsg, s->has_init, s->init, s->init_ttl, s->forcible);
-}
-
-
-int ngx_http_lua_ffi_set_resp_header(ngx_http_request_t *r,
-    const char *key_data, size_t key_len, int is_nil,
-    const char *sval, size_t sval_len, void *mvals,
-    size_t mvals_len, int override, char **errmsg);
 
 typedef struct {
     ngx_http_request_t *r;
@@ -120,6 +74,56 @@ typedef struct {
     char **errmsg;
 } ngx_set_resp_header_t;
 
+/* copied from ngx_http_lua_shdict.c */
+
+int ngx_http_lua_ffi_shdict_get(ngx_shm_zone_t *zone, const unsigned char *key,
+    size_t key_len, int *value_type, unsigned char **str_value_buf,
+    size_t *str_value_len, double *num_value, int *user_flags,
+    int get_stale, int *is_stale, char **errmsg);
+
+int ngx_http_lua_ffi_shdict_store(ngx_shm_zone_t *zone, int op,
+    const unsigned char *key, size_t key_len, int value_type,
+    const unsigned char *str_value_buf, size_t str_value_len,
+    double num_value, long exptime, int user_flags, char **errmsg,
+    int *forcible);
+
+int ngx_http_lua_ffi_shdict_incr(ngx_shm_zone_t *zone, const unsigned char *key,
+    size_t key_len, double *num_value, char **errmsg, int has_init,
+    double init, long init_ttl, int *forcible);
+
+/* copied from ngx_http_lua_headers.c */
+
+int ngx_http_lua_ffi_set_resp_header(ngx_http_request_t *r,
+    const char *key_data, size_t key_len, int is_nil,
+    const char *sval, size_t sval_len, void *mvals,
+    size_t mvals_len, int override, char **errmsg);
+
+int
+ngx_http_lua_ffi_shdict_get_m1(ngx_shdict_get_t *s)
+{
+    return ngx_http_lua_ffi_shdict_get(s->zone, s->key, s->key_len, s->value_type,
+        s->str_value_buf, s->str_value_len, s->num_value, s->user_flags, s->get_stale,
+        s->is_stale, s->errmsg);
+}
+
+
+int
+ngx_http_lua_ffi_shdict_store_m1(ngx_shdict_store_t *s)
+{
+    return ngx_http_lua_ffi_shdict_store(s->zone, s->op, s->key, s->key_len, s->value_type,
+        s->str_value_buf, s->str_value_len, s->num_value, s->exptime, s->user_flags, s->errmsg,
+        s->forcible);
+}
+
+
+int
+ngx_http_lua_ffi_shdict_incr_m1(ngx_shdict_incr_t *s)
+{
+    return ngx_http_lua_ffi_shdict_incr(s->zone, s->key, s->key_len, s->num_value,
+        s->errmsg, s->has_init, s->init, s->init_ttl, s->forcible);
+}
+
+
 int
 ngx_http_lua_ffi_set_resp_header_m1(ngx_set_resp_header_t *s)
 {
@@ -128,4 +132,3 @@ ngx_http_lua_ffi_set_resp_header_m1(ngx_set_resp_header_t *s)
 }
 
 
-// macOS with M1 fixes end
