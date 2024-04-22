83c7249ab3d24c6b06fd1c8220feb129acbb3a52
tzssangglass <tzssangglass@gmail.com>
2024-04-22 12:04:38 +0800

    apply comments
    Signed-off-by: tzssangglass <tzssangglass@gmail.com>

--- lualib/resty/kong/peer_conn.lua
+++ lualib/resty/kong/peer_conn.lua
@@ -5,7 +5,6 @@ local get_request      = base.get_request
 local errmsg           = base.get_errmsg_ptr()
 local C                = ffi.C
 local ffi_str          = ffi.string
-local get_phase        = ngx.get_phase
 local subsystem        = ngx.config.subsystem
 local NGX_ERROR        = ngx.ERROR
 
@@ -16,13 +15,15 @@ local get_last_peer_connection_cached
 
 
 if subsystem == "http" then
+    require "resty.core.phase"  -- for ngx.get_phase
     ffi.cdef[[
     int ngx_http_lua_kong_ffi_get_last_peer_connection_cached(ngx_http_request_t *r,
         char **err);
     ]]
     
     get_last_peer_connection_cached = function()
-        if get_phase() ~= "balancer" then
+        local ngx_phase = ngx.get_phase
+        if ngx_phase() ~= "balancer" then
             error("get_last_peer_connection_cached() can only be called in balancer phase")
         end
     
