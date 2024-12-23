d6e0352af158a1b976a2ee657b18215839f36795
Jun Ouyang <ouyangjun1999@gmail.com>
2024-12-23 17:38:01 +0800

    fix: fix code

--- lualib/resty/kong/upstream.lua
+++ lualib/resty/kong/upstream.lua
@@ -44,55 +44,55 @@ local ffi_str = ffi.string
 local NGX_OK = ngx.OK
 
 local next_upstream_table = {
-	error = C.ngx_http_lua_kong_next_upstream_mask_error,
-	timeout = C.ngx_http_lua_kong_next_upstream_mask_timeout,
-	invalid_header = C.ngx_http_lua_kong_next_upstream_mask_invalid_header,
-	http_500 = C.ngx_http_lua_kong_next_upstream_mask_http_500,
-	http_502 = C.ngx_http_lua_kong_next_upstream_mask_http_502,
-	http_503 = C.ngx_http_lua_kong_next_upstream_mask_http_503,
-	http_504 = C.ngx_http_lua_kong_next_upstream_mask_http_504,
-	http_403 = C.ngx_http_lua_kong_next_upstream_mask_http_403,
-	http_404 = C.ngx_http_lua_kong_next_upstream_mask_http_404,
-	http_429 = C.ngx_http_lua_kong_next_upstream_mask_http_429,
-	off = C.ngx_http_lua_kong_next_upstream_mask_off,
-	non_idempotent = C.ngx_http_lua_kong_next_upstream_mask_non_idempotent,
+    error = C.ngx_http_lua_kong_next_upstream_mask_error,
+    timeout = C.ngx_http_lua_kong_next_upstream_mask_timeout,
+    invalid_header = C.ngx_http_lua_kong_next_upstream_mask_invalid_header,
+    http_500 = C.ngx_http_lua_kong_next_upstream_mask_http_500,
+    http_502 = C.ngx_http_lua_kong_next_upstream_mask_http_502,
+    http_503 = C.ngx_http_lua_kong_next_upstream_mask_http_503,
+    http_504 = C.ngx_http_lua_kong_next_upstream_mask_http_504,
+    http_403 = C.ngx_http_lua_kong_next_upstream_mask_http_403,
+    http_404 = C.ngx_http_lua_kong_next_upstream_mask_http_404,
+    http_429 = C.ngx_http_lua_kong_next_upstream_mask_http_429,
+    off = C.ngx_http_lua_kong_next_upstream_mask_off,
+    non_idempotent = C.ngx_http_lua_kong_next_upstream_mask_non_idempotent,
 }
 
 function _M.set_next_upstream(...)
-	local nargs = select("#", ...)
-	if nargs == 0 then
-		return "no argument"
-	end
+    local nargs = select("#", ...)
+    if nargs == 0 then
+        return "no argument"
+    end
 
-	local r = get_request()
-	if not r then
-		return "no request found"
-	end
+    local r = get_request()
+    if not r then
+        return "no request found"
+    end
 
-	local arg_table = { ... }
-	local next_upstream = 0
-	for i = 1, nargs do
-		local v = arg_table[i]
-		if type(v) ~= "string" then
-			return "argument #" .. i .. " is not a string"
-		end
+    local arg_table = { ... }
+    local next_upstream = 0
+    for i = 1, nargs do
+        local v = arg_table[i]
+        if type(v) ~= "string" then
+            return "argument #" .. i .. " is not a string"
+        end
 
-		local next_upstream_value = next_upstream_table[v]
-		if not next_upstream_value then
-			return "argument #" .. i .. " is not a valid argument"
-		end
+        local next_upstream_value = next_upstream_table[v]
+        if not next_upstream_value then
+            return "argument #" .. i .. " is not a valid argument"
+        end
 
-		next_upstream = bit.bor(next_upstream, next_upstream_value)
-	end
+        next_upstream = bit.bor(next_upstream, next_upstream_value)
+    end
 
-	local err = ffi.new("char *[1]")
-	local rc = C.ngx_http_lua_ffi_set_next_upstream(r, next_upstream, err)
+    local err = ffi.new("char *[1]")
+    local rc = C.ngx_http_lua_ffi_set_next_upstream(r, next_upstream, err)
 
-	if rc ~= NGX_OK then
-		return "failed to set upstream next: " .. ffi_str(err[0])
-	end
+    if rc ~= NGX_OK then
+        return "failed to set upstream next: " .. ffi_str(err[0])
+    end
 
-	return nil
+    return nil
 end
 
 return _M
