5fbea15ef861a70ac4978e703f5804377b62598d
kurt <tzssangglass@gmail.com>
2025-03-14 18:56:16 +0800

    Revert "fix(lib): `resty.kong.peer_conn.*` can only be used in the http subsy…"
    This reverts commit 7da8c7d77b9a33debcc8af2ef1f7cdd0e4bbe952.

--- lualib/resty/kong/peer_conn.lua
+++ lualib/resty/kong/peer_conn.lua
@@ -5,41 +5,36 @@ local get_request      = base.get_request
 local errmsg           = base.get_errmsg_ptr()
 local C                = ffi.C
 local ffi_str          = ffi.string
-local subsystem        = ngx.config.subsystem
+local get_phase        = ngx.get_phase
 local NGX_ERROR        = ngx.ERROR
 
 local error            = error
 
 
-local get_last_peer_connection_cached
+ffi.cdef[[
+int
+ngx_http_lua_kong_ffi_get_last_peer_connection_cached(ngx_http_request_t *r,
+    char **err);
+]]
 
 
-if subsystem == "http" then
-    require "resty.core.phase"  -- for ngx.get_phase
-    local ngx_phase = ngx.get_phase
-    ffi.cdef[[
-    int ngx_http_lua_kong_ffi_get_last_peer_connection_cached(ngx_http_request_t *r,
-        char **err);
-    ]]
-    
-    get_last_peer_connection_cached = function()
-        if ngx_phase() ~= "balancer" then
-            error("get_last_peer_connection_cached() can only be called in balancer phase")
-        end
-    
-        local r = get_request()
-        if not r then
-            error("no request found")
-        end
-    
-        local rc = C.ngx_http_lua_kong_ffi_get_last_peer_connection_cached(r, errmsg)
-    
-        if rc == NGX_ERROR then
-            error(ffi_str(errmsg[0]), 2)
-        end
-    
-        return rc == 1
+local function get_last_peer_connection_cached()
+    if get_phase() ~= "balancer" then
+        error("get_last_peer_connection_cached() can only be called in balancer phase")
     end
+
+    local r = get_request()
+    if not r then
+        error("no request found")
+    end
+
+    local rc = C.ngx_http_lua_kong_ffi_get_last_peer_connection_cached(r, errmsg)
+
+    if rc == NGX_ERROR then
+        error(ffi_str(errmsg[0]), 2)
+    end
+
+    return rc == 1
 end
 
 return {
