f315283408cad36168cf2b6fb6fe111858e7cc6f
Aapo Talvensaari <aapo.talvensaari@gmail.com>
2025-10-24 13:19:47 +0000

    chore(*): update copyright

--- src/ngx_http_lua_kong_grpc.c
+++ src/ngx_http_lua_kong_grpc.c
@@ -1,5 +1,5 @@
 /**
- * Copyright 2019-2022 Kong Inc.
+ * Copyright 2019-2025 Kong Inc.
 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -62,5 +62,3 @@ ngx_http_lua_kong_set_grpc_authority(ngx_http_request_t *r,
         *host = ctx->grpc_authority;
     }
 }
-
-
